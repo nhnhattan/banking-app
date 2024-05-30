be4ec75c08fa42f0bd3922528c44299d0332b02e
Nhật Tân <nhoxcon029@gmail.com>
2024-05-30 17:21:22 +0700

    Update sidebar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,35 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
+
+const Home = () => {
+  const loggedIn = { firstName: "nhnhattan" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[{ currentBalance: 123.5 }, { currentBalance: 500.0 }]}
+        banks={[{}, {}]}
+      />
+    </section>
+  );
+};
+
+export default Home;
